a02fbf9a5c3c58983c696103bf16af81f1edff47
IcaroRaphael <icaro2551@gmail.com>
2025-09-02 07:36:12 -0300

    Versão Inicial

---
+++ PlataformaGestaoFinanceira(ScriptCriacao).sql
@@ -0,0 +1,261 @@
+USE master;
+GO
+
+DROP DATABASE PlataformaGestaoFinanceira;
+GO
+
+CREATE DATABASE PlataformaGestaoFinanceira;
+GO
+
+USE PlataformaGestaoFinanceira;
+GO
+
+-- Usuarios e Controle de Acesso
+CREATE TABLE ContaLogin(
+	Id INTEGER IDENTITY NOT NULL,
+	Email VARCHAR(150) NOT NULL,
+	Senha VARCHAR(150) NOT NULL,
+	DataRegistro DATETIME NOT NULL,
+	DataUltimaAlteracao DATETIME NOT NULL,
+
+	CONSTRAINT PK_ContaLogin PRIMARY KEY (Id),
+	CONSTRAINT UQ_ContaLogin_Email UNIQUE (Email)
+);
+GO
+
+CREATE TABLE PermissaoAcesso(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+	Descricao VARCHAR(500) NOT NULL,
+
+	CONSTRAINT PK_PermissaoAcesso PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE Usuario(
+	Id INTEGER IDENTITY NOT NULL,
+	NomeCompleto VARCHAR(100) NOT NULL,
+	DataCriacao DATETIME NOT NULL,
+	DataAtualizacao DATETIME NOT NULL,
+	IdContaLogin INTEGER NOT NULL,
+	IdPermissaoAcesso TINYINT NOT NULL,
+
+	CONSTRAINT PK_Usuario PRIMARY KEY (Id),
+	CONSTRAINT PK_Usuario_ContaLogin FOREIGN KEY (IdContaLogin) REFERENCES ContaLogin (Id),
+	CONSTRAINT PK_Usuario_PermissaoAcesso FOREIGN KEY (IdPermissaoAcesso) REFERENCES PermissaoAcesso (Id)
+);
+GO
+
+-- Investimentos
+CREATE TABLE StatusInvestimento(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_StatusInvestimento PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE TipoInvestimento(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50),
+
+	CONSTRAINT PK_TipoInvestimento PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE Investimento(
+	Id INTEGER IDENTITY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Descricao VARCHAR(500) NULL,
+	ValorAtual DECIMAL(15,2) NOT NULL,
+	DataInvestimento DATE NOT NULL,
+	IdTipoInvestimento TINYINT NOT NULL,
+	IdStatusInvestimento TINYINT NOT NULL,
+	IdUsuario INTEGER NOT NULL,
+
+	CONSTRAINT PK_Investimento PRIMARY KEY (Id),
+	CONSTRAINT FK_Investimento_TipoInvestimento FOREIGN KEY (IdTipoInvestimento) REFERENCES TipoInvestimento (Id),
+	CONSTRAINT FK_Investimento_StatusInvestimento FOREIGN KEY (IdStatusInvestimento) REFERENCES StatusInvestimento (Id),
+	CONSTRAINT FK_Investimento_Usuario FOREIGN KEY (IdUsuario) REFERENCES Usuario (Id)
+);
+GO
+
+CREATE TABLE HistoricoInvestimento(
+	Id INTEGER IDENTITY NOT NULL,
+	Valor DECIMAL(15,2) NOT NULL,
+	DataAtualizacao DATE NOT NULL,
+	IdInvestimento INTEGER NOT NULL,
+
+	CONSTRAINT PK_HistoricoInvestimento PRIMARY KEY (Id),
+	CONSTRAINT FK_HistoricoInvestimento_Investimento FOREIGN KEY (IdInvestimento) REFERENCES Investimento (Id)
+);
+GO
+
+-- Transações e Contas Financeiras
+CREATE TABLE InstituicaoFinanceira(
+	Id INTEGER IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_InstituicaoFinanceira PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE TipoContaFinanceira(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_TipoContaFinanceira PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE StatusContaFinanceira(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_StatusContaFinanceira PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE ContaFinanceira(
+	Id INTEGER IDENTITY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Descricao VARCHAR(500) NULL,
+	SaldoInicial DECIMAL(15,2) NOT NULL,
+	SaldoAtual DECIMAL(15,2) NOT NULL,
+	LimiteCredito DECIMAL(15,2) NULL,
+	DiaFechamentoFatura TINYINT NULL,
+	DiaVencimentoFatura TINYINT NULL,
+	IdInstituicaoFinanceira INTEGER NOT NULL,
+	IdTipoContaFinanceira TINYINT NOT NULL,
+	IdStatusContaFinanceira TINYINT NOT NULL,
+
+	CONSTRAINT PK_ContaFinanceira PRIMARY KEY (Id),
+	CONSTRAINT FK_ContaFinanceira_InstituicaoFinanceira FOREIGN KEY (IdInstituicaoFinanceira) REFERENCES InstituicaoFinanceira (Id),
+	CONSTRAINT FK_ContaFinanceira_TipoContaFinanceira FOREIGN KEY (IdTipoContaFinanceira) REFERENCES TipoContaFinanceira (Id),
+	CONSTRAINT FK_ContaFinanceira_StatusContaFinanceira FOREIGN KEY (IdStatusContaFinanceira) REFERENCES StatusContaFinanceira (Id)
+);
+GO
+
+CREATE TABLE UsuarioContaFinanceira(
+	IdUsuario INTEGER NOT NULL,
+	IdContaFinanceira INTEGER NOT NULL,
+
+	CONSTRAINT PK_UsuarioContaFinanceiro PRIMARY KEY (IdUsuario, IdContaFinanceira),
+	CONSTRAINT FK_UsuarioContaFinanceira_Usuario FOREIGN KEY (IdUsuario) REFERENCES Usuario (Id),
+	CONSTRAINT FK_UsuarioContaFinanceira_ContaFinanceira FOREIGN KEY (IdContaFinanceira) REFERENCES ContaFinanceira (Id)
+);
+GO
+
+CREATE TABLE TipoTransacao(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_TipoTransacao PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE StatusTransacao(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_StatusTransacao PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE TipoCategoria(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL
+
+	CONSTRAINT PK_TipoCategoria PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE Categoria(
+	Id INTEGER IDENTITY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	IdUsuario INTEGER NULL,
+	IdTipoCategoria TINYINT NOT NULL,
+
+	CONSTRAINT PK_Categoria PRIMARY KEY (Id),
+	CONSTRAINT FK_Categoria_Usuario FOREIGN KEY (IdUsuario) REFERENCES Usuario (Id),
+	CONSTRAINT FK_Categoria_TipoCategoria FOREIGN KEY (IdTipoCategoria) REFERENCES TipoCategoria (Id)
+);
+GO
+
+CREATE TABLE Transacao(
+	Id INTEGER IDENTITY NOT NULL,
+	Descricao VARCHAR(500) NULL,
+	Valor DECIMAL(15,2) NOT NULL,
+	DataTransacao DATETIME NOT NULL,
+	IdContaFinanceira INTEGER NOT NULL,
+	IdUsuario INTEGER NOT NULL,
+	IdCategoria INTEGER NOT NULL,
+	IdTipoTransacao TINYINT NOT NULL,
+	IdStatusTransacao TINYINT NOT NULL,
+
+	CONSTRAINT PK_Transacao PRIMARY KEY (Id),
+	CONSTRAINT FK_Transacao_ContaFinanceira FOREIGN KEY (IdContaFinanceira) REFERENCES ContaFinanceira (Id),
+	CONSTRAINT FK_Transacao_Usuario FOREIGN KEY (IdUsuario) REFERENCES Usuario (Id),
+	CONSTRAINT FK_Transacao_Categoria FOREIGN KEY (IdCategoria) REFERENCES Categoria (Id),
+	CONSTRAINT FK_Transacao_TipoTransacao FOREIGN KEY (IdTipoTransacao) REFERENCES TipoTransacao (Id),
+	CONSTRAINT FK_Transacao_StatusTransacao FOREIGN KEY (IdStatusTransacao) REFERENCES StatusTransacao (Id)
+);
+GO
+
+-- Orçamento e Metas
+CREATE TABLE StatusMetaFinanceira(
+	Id TINYINT IDENTITY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_StatusMetaFinanceira PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE MetaFinanceira(
+	Id INTEGER IDENTITY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Descricao VARCHAR(500) NULL,
+	ValorAlvo DECIMAL(15,2) NOT NULL,
+	ValorAtual DECIMAL(15,2) NOT NULL,
+	DataAlvo DATE NULL,
+	IdStatusMetaFinanceira TINYINT NOT NULL,
+
+	CONSTRAINT PK_MetaFinanceira PRIMARY KEY (Id),
+	CONSTRAINT FK_MetaFinanceira_StatusMetaFinanceira FOREIGN KEY (IdStatusMetaFinanceira) REFERENCES StatusMetaFinanceira (Id)
+);
+GO
+
+CREATE TABLE UsuarioMetaFinanceira(
+	IdUsuario INTEGER NOT NULL,
+	IdMetaFinanceira INTEGER NOT NULL,
+
+	CONSTRAINT PK_UsuarioMetaFinanceira PRIMARY KEY (IdUsuario, IdMetaFinanceira),
+	CONSTRAINT FK_UsuarioMetaFinanceira_Usuario FOREIGN KEY (IdUsuario) REFERENCES Usuario (Id),
+	CONSTRAINT FK_UsuarioMetaFinanceira_MetaFinanceira FOREIGN KEY (IdMetaFinanceira) REFERENCES MetaFinanceira (Id)
+);
+GO
+
+CREATE TABLE OrcamentoFinanceiro(
+	Id INTEGER IDENTITY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Descricao VARCHAR(500) NULL,
+	ValorEstimado DECIMAL(15,2) NOT NULL,
+	ValorGasto DECIMAL(15,2) NULL,
+	Mes TINYINT NULL,
+	Ano SMALLINT NULL,
+	IdCategoria INTEGER NOT NULL,
+
+	CONSTRAINT PK_OrcamentoFinanceiro PRIMARY KEY (Id),
+	CONSTRAINT FK_OrcamentoFinanceiro FOREIGN KEY (IdCategoria) REFERENCES Categoria (Id)
+);
+GO
+
+CREATE TABLE UsuarioOrcamentoFinanceiro(
+	IdUsuario INTEGER NOT NULL,
+	IdOrcamentoFinanceiro INTEGER NOT NULL,
+
+	CONSTRAINT PK_UsuarioOrcamentoFinanceiro PRIMARY KEY (IdUsuario, IdOrcamentoFinanceiro),
+	CONSTRAINT FK_UsuarioOrcamentoFinanceiro_Usuario FOREIGN KEY (IdUsuario) REFERENCES Usuario (Id),
+	CONSTRAINT FK_UsuarioOrcamentoFinanceiro_OrcamentoFinanceiro FOREIGN KEY (IdOrcamentoFinanceiro) REFERENCES OrcamentoFinanceiro (Id)
+);
+GO
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # plataforma-gestao-financeira-documentacao
+
+
+Link da Modelagem: https://www.drawdb.app/editor?shareId=3dab63cba09f844d1c405c3981ad9e7b
